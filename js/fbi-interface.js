3ea65f2c2885a1521a2dbadbe1b8e2d7858b7075
Margaret Berry and Nate McGregor <margaret@>
2017-10-18 15:37:00 -0700

    fix per page error

--- js/fbi-interface.js
+++ js/fbi-interface.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
 
     let promise = new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.usa.gov/crime/fbi/ucr/estimates/states/OR?page=1&per_page=10&output=json&api_key=iiHnOKfno2Mgkt5AynpvPpUQTEyxE77jo1RU8PIv`;
+      let url = `https://api.usa.gov/crime/fbi/ucr/estimates/states/OR?page=1&per_page=23&output=json&api_key=iiHnOKfno2Mgkt5AynpvPpUQTEyxE77jo1RU8PIv`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
